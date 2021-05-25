20f5560272e7501a65c2cc7a9ff9a58ac3319268
sqohapoe <victorlin06198@gmail.com>
2021-05-25 20:24:33 +0200

    add liukeymatcher

---
+++ src/liukey_matcher.py
@@ -0,0 +1,65 @@
+import keyboard
+import logging
+
+
+# reset event queue if weird stuff is pressed
+# change event queue when backspace pressed
+    
+class LiukeyMatcher:
+    VALID_NAME = set('abcdefghijklmnopqrstuvwxyz,.')
+    
+    def __init__(self, liu_dict=None, loader=None, test_mode=False):
+        self.event_queue = []
+        self.test_mode = test_mode # when in test mode, don't send backspaces
+        
+        if loader is not None:
+            loader.load()
+            self.LIU_DICT = loader.liu_dict
+        elif liu_dict is not None:
+            self.LIU_DICT = liu_dict
+        else:
+            self.LIU_DICT = dict()
+            logging.info("No loader or liu_dict is used")
+    
+    def __call__(self, kbe: keyboard.KeyboardEvent):
+        # TODO handle backspace
+        if kbe.event_type == 'up':
+            return None
+        
+        if kbe.name in self.VALID_NAME:
+            self.event_queue.append(kbe)
+            
+        elif kbe.name == 'space':
+            msg = self.get_match()
+            keyboard.write(msg)
+            self.event_queue = []
+        
+        elif kbe.name == 'backspace':
+            try:
+                logging.debug(f"popped last from {self.event_queue}")
+                self.event_queue.pop()
+            except IndexError:
+                pass
+            
+        else: # kbe.name not valid
+            self.event_queue = []
+            
+            
+        
+    
+    def get_backspaces(self, abbrev):
+        return '\b'*(1+len(abbrev))
+    
+    def get_match(self):
+        filtered_events = filter(lambda kbe: kbe.event_type == 'down', self.event_queue)
+        keys_pressed = map(lambda kbe: kbe.name, filtered_events)
+        abbrev = ''.join(keys_pressed)
+        logging.debug(abbrev)
+        if abbrev in self.LIU_DICT.keys():
+            return self.get_backspaces(abbrev) +  self.LIU_DICT[abbrev]
+        else:
+            return ''
+        
+    def hook(self):
+        keyboard.hook(self)
+        
--- src/main.py
+++ src/main.py
@@ -6,13 +6,20 @@ logging.basicConfig(level=logging.DEBUG)
 
 from abbrev_loader import AbbrevLoader
 from stenokey_matcher import StenokeyMatcher
+from liukey_matcher import LiukeyMatcher
 
 
 # changelog : Liukey functionality removed for simplicity
 
 def main(wait_for=5):
     sm = StenokeyMatcher(loader=AbbrevLoader())
-    sm.hook()
+    lm = LiukeyMatcher(loader=AbbrevLoader())
+    # sm.hook()
+    # lm.hook()
+    def caller(kbe : keyboard.KeyboardEvent):
+        sm(kbe)
+        lm(kbe)
+    keyboard.hook(caller)
     time.sleep(wait_for)
     
     
