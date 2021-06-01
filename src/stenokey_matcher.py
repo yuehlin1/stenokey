38565e0140539ad4aa6d17488919e8e405fd3e2b
sqohapoe <victorlin06198@gmail.com>
2021-06-01 20:06:05 +0200

    first commit gui
    abbre_loader : # for comment
    gui : OO style App window
    main : main_gui
    stenokey_matcher : add unhook method and load method

--- src/stenokey_matcher.py
+++ src/stenokey_matcher.py
@@ -11,6 +11,16 @@ class StenokeyMatcher:
         self.event_queue = []
         self.test_mode = test_mode # when in test mode, don't send backspaces
         
+        self._steno_dict = steno_dict
+        self._loader = loader
+        
+        self.load()
+        
+        
+            
+    def load(self):
+        steno_dict = self._steno_dict
+        loader = self._loader
         if loader is not None:
             loader.load()
             self.STENO_DICT = loader.steno_dict
@@ -19,6 +29,7 @@ class StenokeyMatcher:
         else:
             self.STENO_DICT = dict()
             logging.info("No loader or steno_dict is used")
+            
     
     def __call__(self, kbe: keyboard.KeyboardEvent):
         logging.debug("steno call")
@@ -106,5 +117,8 @@ class StenokeyMatcher:
             return ''
         
     def hook(self):
-        keyboard.hook(self)
+        self.handler = keyboard.hook(self)
+        
+    def unhook(self):
+        keyboard.unhook(self.handler)
         
