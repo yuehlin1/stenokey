19972a25efab9f2070d69d4808dfbfd1768c392f
sqohapoe <victorlin06198@gmail.com>
2021-06-12 13:05:34 +0200

    fix shift hotkey

--- src/main.py
+++ src/main.py
@@ -17,17 +17,32 @@ from gui import GUI
 def main(wait_for=5):
     sm = StenokeyMatcher(loader=AbbrevLoader())
     lm = LiukeyMatcher(loader=AbbrevLoader())
-    mng = CombokeyToggleManager(sm, lm, root=None)
+    mng = CombokeyToggleManager(sm, lm)
     mng.toggle_liu()
     mng.toggle_liu()
     time.sleep(wait_for)
+    
+class StenoHotkeyManager:
+    def __init__(self, app):
+        self.app = app
+        
+    def __call__(self, kbe):
+        if kbe.name == "shift" and kbe.event_type == "up": # if shift is the only one being pressed
+            return self.app.stenokey_button.invoke()
     
 def main_gui():
     sm = StenokeyMatcher(loader=AbbrevLoader())
     lm = LiukeyMatcher(loader=AbbrevLoader())
     mng = CombokeyToggleManager(sm, lm)
     app = GUI(mng)
+    shm = StenoHotkeyManager(app)
+    keyboard.hook(shm)
+    
     app.toggle_show_gui_hotkey_set("ctrl+space")
+    
+    
+    
+    
     app.mainloop()
     
 if __name__ == "__main__":
